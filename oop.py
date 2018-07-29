a551ba29a5446a53e24397523da284296b4efe16
lievi <lievi.silva@gmail.com>
2018-07-29 20:23:52 -0300

    aula 6 - Property decorator

--- oop.py
+++ oop.py
@@ -7,36 +7,38 @@ class Employee(object):
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = '{}.{}@company.com'.format(self.first, self.last)
 
         Employee.num_of_emps += 1
 
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def __repr__(self):
-        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
-
-    def __add__(self, other):
-        return self.pay + other.pay
-
-    def __len__(self):
-        return len(self.fullname())
+    @property
+    def email(self):
+        return '{}.{}@company.com'.format(self.first, self.last)
 
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('John', 'Doe', 60000)
 
-print(len(emp_1))
+emp_1.fullname = 'Corey Schafer'
 
-print(emp_1 + emp_2)
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
 
-# print(emp_1)
-# print(emp_1.__repr__())
-# print(emp_1.__str__())
+del(emp_1.fullname)
+del(emp_2.first)
