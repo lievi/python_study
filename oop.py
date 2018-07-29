389f12e536265ada5bb97476338781d1e35c53f5
lievi <lievi.silva@gmail.com>
2018-07-29 17:33:49 -0300

    aula 2 - Variáveis de classe

--- oop.py
+++ oop.py
@@ -1,4 +1,7 @@
 class Employee(object):
+    # Class variable
+    num_of_emps = 0
+    raise_amount = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -6,11 +9,16 @@ class Employee(object):
         self.pay = pay
         self.email = '{}.{}@company.com'.format(self.first, self.last)
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('John', 'Doe', 70000)
 
-print(emp_1.fullname())
+print(Employee.num_of_emps)
