be104ba7cfeca4620aaa2f774de0500d566ac68a
lievi <lievi.silva@gmail.com>
2018-07-29 18:37:55 -0300

    aula 4 - class method and static method

--- oop.py
+++ oop.py
@@ -17,8 +17,48 @@ class Employee(object):
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        """Contrutor alternativo que recebe os parâmetros como string.
+
+        Arguments:
+            emp_str {str} -- A string com os parâmetros separados por hifén
+
+        Returns:
+            Employee -- Nova instância da classe employee
+
+        """
+        first, last, pay = emp_str.split('-')
+        # Retornando uma nova instância da classe (cls, tipo um self)
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        """
+        Exemplo de função estática.
+
+        A função estática é uma função que não precisa do self ou cls
+        É somente algum pedaço de lógica que faz sentido para a classe.
+
+        Arguments:
+            day {datetime} -- O dia para ser verificado
+
+        Returns:
+            Boolean -- Se é workday ou não
+
+        """
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('John', 'Doe', 70000)
+emp_2 = Employee('John', 'Doe', 60000)
 
-print(Employee.num_of_emps)
+import datetime
+my_date = datetime.date(2006, 7, 10)
+print(Employee.is_workday(my_date))
