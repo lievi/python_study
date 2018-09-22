e4e573e34a0da573020de327ca1ce8d2abc4a64c
lievi <lievi.silva@gmail.com>
2018-09-22 15:28:39 -0300

    Python OOP - Inheritance

--- oop.py
+++ oop.py
@@ -1,44 +1,79 @@
-class Employee(object):
-    # Class variable
-    num_of_emps = 0
-    raise_amount = 1.04
+from typing import List
 
-    def __init__(self, first, last, pay):
+
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first: str, last: str, pay: int):
+        """Função construtora da classe de emprego.
+
+        Arguments:
+            first {str} -- primeiro nome
+            last {str} -- Ultimo nome
+            pay {int} -- Valor pago
+        """
         self.first = first
         self.last = last
+        self.email = "{}.{}@email.com".format(
+            self.first.lower(), self.last.lower())
         self.pay = pay
 
-        Employee.num_of_emps += 1
+    def fullname(self) -> str:
+        """Retorna o Nome e Sobrenome do empregado.
+        """
+        return "{} {}".format(self.first.capitalize(), self.last.capitalize())
 
-    @property
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
-
-    @fullname.setter
-    def fullname(self, name):
-        first, last = name.split(' ')
-        self.first = first
-        self.last = last
-
-    @fullname.deleter
-    def fullname(self):
-        print('Delete Name!')
-        self.first = None
-        self.last = None
-
-    @property
-    def email(self):
-        return '{}.{}@company.com'.format(self.first, self.last)
+    def apply_raise(self):
+        """Aplica aumento no salário do empregado.
+        """
+        self.pay = int(self.pay * self.raise_amt)
 
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('John', 'Doe', 60000)
+class Developer(Employee):
+    raise_amt = 1.10
 
-emp_1.fullname = 'Corey Schafer'
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-print(emp_1.first)
-print(emp_1.email)
-print(emp_1.fullname)
 
-del(emp_1.fullname)
-del(emp_2.first)
+class Manager(Employee):
+
+    def __init__(self, first: str, last: str, pay: int, employees: List[Employee] = None):
+
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, employee):
+        if employee not in self.employees:
+            self.employees.append(employee)
+
+    def remove_emp(self, employee):
+        if employee in self.employees:
+            self.employees.remove(employee)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print("-->", emp.fullname())
+
+
+dev_1 = Developer("lievi", "silva", 50000, "python")
+dev_2 = Developer("John", "DOE", 30000, 'C#')
+
+mng_1 = Manager("Sue", "smith", 90000, "nhjas")
+
+print(mng_1.email)
+mng_1.add_emp(dev_2)
+mng_1.remove_emp(dev_1)
+mng_1.print_emps()
+
+# print(dev_1.fullname())
+# print(dev_2.prog_lang)
+
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
